04e5d72168a76a781b40e003ce630527f6754a54
William Lafond <lafonwi1@corp.onespan.com>
2025-05-17 00:47:30 -0400

    refactored vectors<>

--- src/hardware/chip8.h
+++ src/hardware/chip8.h
@@ -5,24 +5,21 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 #include <cstdint>
+#include <vector>
 
 namespace Chip8 {
     class Chip8 {
     public:
-        uint8_t registers[16];
-        uint8_t memory[4096];
+        std::vector<uint8_t> registers; //uint8_t registers[16];
+        std::vector<uint8_t> memory;//uint8_t memory[4096];
         uint16_t index_reg;
         uint16_t program_ctr;
-        uint16_t stack[16];
+        std::vector<uint16_t> stack; //uint16_t stack[16];
         uint8_t stack_ptr;
-
         uint8_t delay_timer;
         uint8_t sound_timer;
-
-        uint8_t gfx[64 * 32];
-
+        std::vector<std::vector<uint8_t>> gfx; //uint8_t gfx[64 * 32];
     private:
-
 
     };
 } // Chip8
